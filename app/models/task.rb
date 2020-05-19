644656a39979bbc9bc4757cffd75f7edd084f883
Yann Klein <yann.klein@me.com>
2020-05-19 20:19:14 +0900

    mini livecode doen

---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+# Tasks with title, description and done attributes
+class Task < ActiveRecord::Base
+
+end
---
+++ db/migrate/20200519193656_add_timestamps_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTimestampsToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_timestamps(:tasks)
+  end
+end
